fc762802af96c8b2fbd0e56282f27be089995c70
kafisatz <kafisatz@gmail.com>
2018-06-14 17:20:51 +0200

    removed exported fn

--- src/types.jl
+++ src/types.jl
@@ -2,7 +2,7 @@
 import Base: length,start,next,done,eltype,==,hash
 
 export Splitdef,Rulepath,Leaf,Node,Tree,BoostedTree,SplittingCriterion,CVOptions,BaggedTree
-export ModelSettings,updateSettings!,copySettingsToCurrentType
+export ModelSettings,copySettingsToCurrentType
 
 struct pdaMod end #this is a legacy type. It is only defined here, because we have some 'old' unused functions in the code which have a singature with this type (and the signature needs an update....)
     
