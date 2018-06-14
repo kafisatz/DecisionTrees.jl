3d64380144047ee0cff7fb4c79bde2b03e43f423
kafisatz <kafisatz@gmail.com>
2018-06-14 12:11:15 +0200

    added createGridSearchSettings

--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -1,4 +1,4 @@
-export poissonError,confusmatBinary,confusmat,resample_trnvalidx!,removeBOM,sampleData,subset_pda_mod #for debugging purposes only
+export createGridSearchSettings,poissonError,confusmatBinary,confusmat,resample_trnvalidx!,removeBOM,sampleData,subset_pda_mod #for debugging purposes only
 
 import Base: append!,mean,length,findin,isless,eltype,resize!,convert
 #import PooledArrays.levels
@@ -5405,4 +5405,54 @@ function poissonError(trueNumerator,exposure,estimatedFrequency)
     #errs=poissonError(dtmtable.numerator,dtmtable.weight,fitted);
     #sum(errs[dtmtable.trnidx])/length(dtmtable.trnidx)
     #sum(errs[dtmtable.validx])/length(dtmtable.validx)
+end
+
+
+function createGridSearchSettings(sett::ModelSettings;args...)
+    settingsVector=[sett]
+	seen=[]
+	for (smember,valuelist) in args
+		if in(smember,seen)
+			error("You provided multiple values for the field $(smember)")
+		end
+        push!(seen,smember)
+        try 
+            sz=size(valuelist)
+            @assert length(sz)==1 "DTM: expected a vector for $(smember). You provided $(typeof(valuelist)). \r\n$(valuelist)"
+            n=sz[1]
+            @assert n>0
+            
+            settingsVectorTemplate=deepcopy(settingsVector)
+            
+            for i=1:n
+                #set value for this 'batch'  
+                v=valuelist[i]        
+                thisSettingsBatch=deepcopy(settingsVectorTemplate)      
+                for kk=1:length(thisSettingsBatch)
+                    s=thisSettingsBatch[kk]
+                    #updateSettingsMod!(s,smember=)
+                    oldValue=getfield(s,smember)
+                    if typeof(v)!=typeof(oldValue)
+                        valConverted=convertFromString(oldValue,string(v)) #tbd maybe string(v) could/should be replaced with v here
+                        setfield!(s,smember,valConverted)
+                    else
+                        setfield!(s,smember,v)
+                    end		
+                    res=checkIfSettingsAreValid(s)
+                    @assert res==nothing "DTM: Some settings are invalid! Abort."
+                end
+                if i==1
+                    settingsVector=deepcopy(thisSettingsBatch)
+                else 
+                    append!(settingsVector,thisSettingsBatch)
+                end    
+            end
+        catch e            
+            @show smember,size(valuelist)
+            @show valuelist
+            @show e
+            throw("DTM: unable to create settings vector.")
+        end
+    end
+    return settingsVector
 end
