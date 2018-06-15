7223901f03e5013de1718acd25a7d492cd7f2da4
kafisatz <kafisatz@gmail.com>
2018-06-15 14:10:58 +0200

    added profiling.jl

--- testing/Profiling.jl
+++ testing/Profiling.jl
@@ -1,61 +1,47 @@
-this_dir=string(dictGlobalSettings["Julia Code Folder"],"\\src");cd(this_dir);push!(LOAD_PATH,this_dir)
-chosenj="tree_small" #this runs a small version to compile all functions
-using DTM
-#using Graphics,ProfileView
-using ProfileView
+using Revise
+using Profile
+using CSV,DataFrames
+using DecisionTrees  
 
-#this runs a small version to compile all functions
-loc="C:\\Users\\tbd\\Documents\\Arbeitsordner (sync)\\Public NL\\Personal\\Bernhard\\Resources\\Milliman Software\\Predictive Analytics Paris\\Training\\data\\"
-loc=string(dictGlobalSettings["Data Folder"],"JuliaTestData\\")
-ARGS=[string(loc,"jmortgage.settings_profiling.csv") string(loc,"jmortgage.jld") string("out_jmortgage")];
-resbool=DTM.run_model(ARGS) #small version to compile functions
-@time resbool=DTM.run_model(ARGS)
+include("test\\runtests.jl")
 
-#chosenj="julia200k";ARGS=[string("a:\\JuliaTestData\\",chosenj,".settings.csv") string("a:\\JuliaTestData\\",chosenj,".csv") string("out_",chosenj)];
-#@time resbool=DTM.run_model(ARGS)
-#chosenj="julia200k_200s";ARGS=[string("a:\\JuliaTestData\\",chosenj,".settings2.csv") string("a:\\JuliaTestData\\",chosenj,".csv") string("out_",chosenj)];
-#juliaresult=DTM.run_model(ARGS)
-chosenj="julia400k";ARGS=[string("a:\\JuliaTestData\\",chosenj,".settings2.csv") string("a:\\JuliaTestData\\",chosenj,".csv") string("out_",chosenj)];
-@time juliaresult=DTM.run_model(ARGS)
-chosenj="julia400k";ARGS=[string("a:\\JuliaTestData\\",chosenj,".settings2.csv") string("a:\\JuliaTestData\\",chosenj,".jld") string("out_",chosenj)];
-@time juliaresult=DTM.run_model(ARGS)
+datafile=string("data\\freMTPL2\\freMTPL2.csv")
+@assert isfile(datafile);
+@time fullData=CSV.read(datafile,rows_for_type_detect=100000,allowmissing=:none,categorical=false);
 
-println("**************************************************\n\n\nStarting Profiling....")
+areasSorted=sort(unique(fullData[:Area]))
+AreaInteger=map(x->findall((in)([x]),areasSorted)[1],fullData[:Area])
+fullData[:AreaInteger]=AreaInteger
+
+#correct for unreasonable observations
+for i=1:size(fullData,1)
+    fullData[:ClaimNb][i]=min(4,fullData[:ClaimNb][i])
+    fullData[:Exposure][i]=min(1.0,fullData[:Exposure][i])
+end
+    
+#set independent variables
+selected_explanatory_vars=["Area","AreaInteger","VehPower","VehAge","DrivAge","BonusMalus","VehBrand","VehGas","Density","Region"]
+
+dtmtable,sett,dfprepped=prepare_dataframe_for_dtm!(fullData,trnvalcol="trnTest",numcol="ClaimNb",denomcol="Exposure",weightcol="Exposure",independent_vars=selected_explanatory_vars);
+
+updateSettingsMod!(sett,minw=-0.03,model_type="boosted_tree",niter=40,mf=0.025,subsampling_features_prop=.7,boolCalculatePoissonError=true)
+
+resultingFiles,resM=dtm(dtmtable,sett)
+
+@profile dtm(dtmtable,sett)
 
 
-Profile.clear()          # in case we have any previous profiling data
-Profile.init(10^7, 0.01) #n,delay #Profiling settings
-@profile juliaresult=DTM.run_model(ARGS)   # Profile the program
-# profile() #this shows profile graphically in Juno
+li, lidict = Profile.retrieve()
+using JLD2
+@save "/tmp/foo.jlprof" li lidict
 
-val=ProfileView.view()
+#open("C:\\temp\\logs\\profile.txt", "w") do s
+#    Profile.print(IOContext(s, :displaysize => (24, 500)))
+#end
 
 #=
-	@profile df=DTM.mysql_execute_query_DF_wo_NA(con,command);
-	val=ProfileView.view()
-
-	@profile df=DTM.mysql_execute_query(con,command);
-	val=ProfileView.view()
-
-	Profile.clear()          # in case we have any previous profiling data
-	h=x*.23+9
-	@profile juliaresult=DTM.gini(x,h)   # Profile the program
-	val=ProfileView.view()
-# profile() #this shows profile graphically in Juno
-
-val=ProfileView.view()
-
-=#
-#=
-	val2=ProfileView.view(Profile.fetch(); format = :tree,lidict = nothing, C = false, colorgc = true, fontsize = 12, combine = true)
-
-	fi="c:\\temp\\myprof.txt"
-	isfile(fi)&&rm(fi)
-	f=open(fi,"w");
-		Profile.print(f,format=:flat,combine=true,cols=1000,combine=true);
-	close(f);
-	f=open(fi,"w")
-		Profile.print(f,format=:flat,combine=true,cols=1000,combine=false)
-	close(f)
-	d,l=Profile.retrieve();
+#to open and read the data
+using HDF5, JLD, ProfileView
+@load "/tmp/profdata.jld"
+ProfileView.view(li, lidict=lidict)
 =#
