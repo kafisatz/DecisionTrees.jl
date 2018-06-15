ff6ba455b84c276a00935ae10a03f2b954e3c22b
b <bernhard.koenig@milliman.com>
2018-06-15 18:27:23 +0200

    minor update

--- tutorials/frenchMPTLdata_single_tree.jl
+++ tutorials/frenchMPTLdata_single_tree.jl
@@ -97,6 +97,11 @@ dtmtable.features #explanatory variables
 #we realize that it may be sutiable to define the splitting in a different manner (than uniformly spaced).
 #this feature might be added at a later time. You can consider the function add_coded_numdata!(...) to see how splitting points are chosen
 
+#We note that 'pooling' the data into X (up to sett.max_splitting_points_num) buckets has a few advantages:
+#one can do sorting in linear time (countsort)
+#the data takes up less space in RAM and caches which generally increases performance
+
+#keep a copy of original train and test split
 originalTrnValIndex=deepcopy(fullData[:trnTest])    
 
 #Redefine trn and val data sets
@@ -140,12 +145,19 @@ fitted,leafnumbers=predict(resM,dtmtable.features)
 unique(fitted)
 unique(leafnumbers)
 
+#todo: add the poisson error calc here in julia
+#notably, we are using the difference as splitting criterion, which has (to our knowledge) no explicit relationship
+#to the poisson distribution which is often assumed for count (and frequency) data.
+
 #the leaves are numbered in such a manner that the lowest fitted value corresponds to leaf number 1
 #conversly, the highest fitted value corresponds to the highest leaf number.
 
 ############################################################
 #Additional options
 ############################################################
+
+#Changing the impurity function
+#this is detailed further below
 
 #If you have Graphviz installed, you can generate a plot (on a PDF) of the tree
 updateSettingsMod!(sett,write_dot_graph="true",graphvizexecutable="c:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe")
@@ -279,3 +291,48 @@ CVresult=dtm_single_threaded(dtmtable,sett,cvsampler)
 #let us try to increase the size of the validation data
 #7 fold cross valiation
 CVresult=dtm_single_threaded(dtmtable,sett,CVOptions(-7,0.0,true))
+
+#the averaged error across the validation sets is around 0.314 which does not look unreasonable
+
+#let us creat additional cross valiation models
+#we create 20 models (which randomly sampled data) where the
+CVresult=dtm_single_threaded(dtmtable,sett,CVOptions(20,0.7,true))
+#the error measure remains relatively stable across several CV samples
+#for some samples it is even less than 0.31 on the validation data
+
+
+
+############################################################
+#Changing the impurity function
+############################################################
+
+#Until now we have used the 'difference' measure to select the best split
+#Computationally the difference has the favorable property that we only need 'one pass' over the data
+#That is to say, we can go once over the data and sum up the numerator and denominator for each bucket (in linear time)
+#then, to evaluate each of the possible M splits, we only need to do M-1 comparisons for numerical variables (for categorical variables the case is more complex)
+
+#Let us consider the poisson error
+#to evaluate the poisson error for a given split, we need the mean estimated frequency in the left and right node
+#to obtain these means, we need a pass over the data
+#this increase the computation time considerably compared to the difference measure
+
+#We can set the poisson error as follows
+updateSettingsMod!(sett,crit="_poissondeviance")
+resultingFiles,resM=dtm(dtmtable,sett)
+#For comparison reasons
+#on my machine the above model took 15.8 seconds (only the build tree operation)
+#with the difference measure, it takes about 1.46 seconds
+
+#Note: We have only marginally performace optimized this package. 
+#It is highly likely that a skilled programmer could improve various bits of the code in this package.
+
+#Upon inspecting the resulting errors, we can see a comparable performace as with the difference measure.
+
+#Let us consider a grid search for the Poisson Error impurity function.
+minweight_list=-collect(range(0.0001,stop=0.1,length=25))
+
+settV=createGridSearchSettings(sett,    
+    minw=minweight_list
+    );
+
+gridResult=dtm(dtmtable,settV,file="R:\\temp\\13\\MTPLsingleTree.CSV")
