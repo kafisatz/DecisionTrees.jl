9b4c15f66b67b3728f782e3df4012016923613ea
b <bernhard.koenig@milliman.com>
2018-06-15 14:52:25 +0200

    added sorting.jl for perf testing

---
+++ testing/sorting.jl
@@ -0,0 +1,18 @@
+#sorting.jl
+
+using BenchmarkTools
+
+nn=600000
+
+    raw_rel=rand(nn);
+    raw_rel1=deepcopy(raw_rel);
+    raw_rel2=deepcopy(raw_rel);
+    srt=zeros(Int64,nn);
+
+    srt=zeros(Int64,nn);
+    @benchmark sortperm!(srt,raw_rel1,alg=QuickSort)
+    srt2=zeros(Int64,nn);
+    @benchmark DecisionTrees.my_sortperm_you_should_regularily_check_if_sortperm_in_base_has_become_more_efficient!(srt2,raw_rel) #we could use RadixSort here, it uses about 40m of memory for 5m obs, AND I am unsure how easy it is to get the permutation (compared to sorting in place). But the algorithm is about 50% faster for random floats than QuickSort.    
+    nothing
+
+    
