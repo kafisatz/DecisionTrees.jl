c41e7d3ef149469642479933aca2389bad3d9fef
kafisatz <kafisatz@gmail.com>
2018-06-17 21:23:11 +0200

    minor updates

--- testing/snoopcompile.jl
+++ testing/snoopcompile.jl
@@ -5,14 +5,14 @@ using SnoopCompile
 ### Log the compiles
 # This only needs to be run once (to generate "/tmp/images_compiles.csv")
 
-SnoopCompile.@snoop "/tmp/dtm_compiles.csv" begin
+SnoopCompile.@snoop "dtm_compiles.csv" begin
     include(Pkg.dir("DecisionTrees", "test","runtests.jl"))
 end
 
 ### Parse the compiles and generate precompilation scripts
 # This can be run repeatedly to tweak the scripts
 
-data = SnoopCompile.read("/tmp/dtm_compiles.csv")
+data = SnoopCompile.read("dtm_compiles.csv")
 
 pc = SnoopCompile.parcel(reverse!(data[2]))
-SnoopCompile.write("/tmp/precompile", pc)
+SnoopCompile.write("precompile", pc)
