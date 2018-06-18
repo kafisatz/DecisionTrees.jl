c5e2f81804d8d726da3c77fc6b8e96f772f02919
kafisatz <kafisatz@gmail.com>
2018-06-18 13:37:04 +0200

    update

--- tutorials/frenchMPTLdata_boosting.jl
+++ tutorials/frenchMPTLdata_boosting.jl
@@ -11,7 +11,7 @@
 #Load packages
 ##############################
 t0=time_ns()
-cd(string(ENV["HOMEPATH"],"\\Documents\\ASync\\home\\Code\\Julia\\DecisionTrees.jl"))
+cd(joinpath(GLOBAL_julia_code_folder,"DecisionTrees.jl"))
 #@info("You may want to run 'pkg> instantiate' when you first run this. Use ] to enter the package mode.")
 
 @everywhere using Revise
