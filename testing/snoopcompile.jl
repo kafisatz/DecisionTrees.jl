5dca4943687d03fa186e8a885e4c55a85da22a3c
b <bernhard.koenig@milliman.com>
2018-06-18 11:58:36 +0200

    some updates

--- testing/snoopcompile.jl
+++ testing/snoopcompile.jl
@@ -6,18 +6,23 @@ using SnoopCompile
 # This only needs to be run once (to generate "/tmp/images_compiles.csv")
 
 
-snoopfile="C:\\temp\\_snoop_dtm_compiles.csv"
-SnoopCompile.@snoop snoopfile begin
-    fn=joinpath(dirname(@__FILE__),"..","test","runtests.jl")
-    #fn=Pkg.dir("DecisionTrees", "test","runtests.jl")
-    @assert isfile(fn)
-    include(fn)
+#snoopfile="C:\\temp\\__snoop_dtm_compiles.csv"
+#@assert isdir(splitdir(snoopfile)[1])
+#fn=joinpath(dirname(@__FILE__),"..","test","runtests.jl")
+#if isfile(fn)
+#else 
+#    fn=Pkg.dir("DecisionTrees", "test","runtests.jl")    
+#end
+#@assert isfile(fn)
+
+SnoopCompile.@snoop "C:\\temp\\__snoop_dtm_compiles.csv" begin
+    include(Pkg.dir("DecisionTrees", "test","runtests.jl")    )
 end
 
 ### Parse the compiles and generate precompilation scripts
 # This can be run repeatedly to tweak the scripts
 
-data = SnoopCompile.read(snoopfile)
+data = SnoopCompile.read("C:\\temp\\__snoop_dtm_compiles.csv")
 
 pc = SnoopCompile.parcel(reverse!(data[2]))
 SnoopCompile.write("c:\\temp\\precompile", pc)
