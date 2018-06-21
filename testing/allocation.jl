5a03a9bd63d43dd7754a1d38c47c9000ab174bb0
bernhard <github@diro.ch>
2018-06-21 09:26:36 +0200

    update travis

--- testing/allocation.jl
+++ testing/allocation.jl
@@ -1,21 +1,54 @@
-#this_dir="C:\\Users\\bernhard.konig\\Documents\\ASync\\irobot\\TariffWatch New Repo\\algorithms\\Julia\\Code\\";
-#this_dir="R:\\TariffWatch New Repo\\algorithms\\Julia\\Code\\dev\\src"
-#unshift!(LOAD_PATH,string(this_dir,"\\DTM2_dev\\src"))
-#unshift!(LOAD_PATH,string(this_dir,"\\dev\\src"))
-using Compat
+
 using BenchmarkTools
 using Revise    
 using DecisionTrees
-using DataFrames,JLD2
-datafolder="C:\\temp\\";outfilename="C:\\temp\\DTM2Model";dataFilename="c:\\temp\\DTM2Model.jld";outfileStringOnly="DTM2Model";fn=string(datafolder,outfileStringOnly)
-tmpfilei="C:\\temp\\i.csv";isfile(tmpfilei)&&rm(tmpfilei)
-#=
-    include("C:\\Users\\bernhard.konig\\Documents\\ASync\\home\\Code\\Julia\\DecisionTrees.jl\\testing\\define_df_prepped.jl");
-    df_prepped_orig=deepcopy(df_prepped);
-    #df_prepped=df_prepped_orig[1:2000,:];
-    df_prepped=deepcopy(df_prepped_orig)
-    @time dtmtable=DTM2.prep_data_from_df(df_prepped,this_dt2msett,fn);
-=#
+using DataFrames
+using JLD2
+
+pkgdir=Pkg.dir("DecisionTrees") #how long will this remain supported?
+testdir=joinpath(pkgdir,"test")
+datadir=joinpath(pkgdir,"data")
+
+elt=[Int64,	Float64,	Float64,	Float64,	Float64,	Float64,	Int64,	String,	String,	String,	Int64,	String,	String,	String,	String,	String,	String,	String,	String,	String,	String,	Int64,	String,	Int64,	String,	String,	Int64,	String,	String,	Int64,	String,	String,	Int64,	String,	String,	Int64,	String,	Int64,	String,	String,	Int64,	Int64,	String,	Int64,	Int64,	String,	Int64,	Int64,	String,	Int64,	String,	String,	String,	Int64,	String,	String,	String,	String,	Int64,	Int64,	Int64,	String,	String,	String,	String,	String,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64,	Float64,	Int64,	Float64,	Float64,	Float64,	Float64,	Int64,	Int64,	Int64,	Int64,	Int64,	Int64]
+
+fi="data1Small.csv"
+thisfile=joinpath(datadir,fi)
+@test isfile(thisfile)
+@time df_tmp=CSV.read(thisfile,allowmissing=:none,types=elt,categorical=false,rows_for_type_detect=10000);
+
+selected_explanatory_vars=["PLZ_WOHNORT","ART_DES_WOHNEIGENTUM","GEBURTSDATUM","FAMILIENSTAND","NATIONALITAET","GESCHLECHT","FINANZIERUNGSART","STADT","KENNZEICHEN"]
+
+#keep 10 largest PLZ only
+vorig=deepcopy(df_tmp[:PLZ_WOHNORT])
+counts,freqs,vals,keep=getCounts(vorig,threshold=10)
+vnew=df_tmp[:PLZ_WOHNORT]
+mapToOther!(vnew,keep,9999999)
+
+##################################################
+#Prepare the data
+##################################################
+
+dtmtable,sett,df_prepped=prepare_dataframe_for_dtm!(df_tmp,treat_as_categorical_variable=["PLZ_WOHNORT"],weightcol="EXPOSURE",numcol="LOSS20HALF",denomcol="PREMIUM66",independent_vars=selected_explanatory_vars);
+sett.minw=-.2
+
+#selected_explanatory_vars=[    "VORSCHAEDEN_ANZAHL",    "MALLORCA_POLICE",	"SCHUTZBRIEF_INKL",	"FREIE_WERKSTATTWAHL",	"AUTOMOBILCLUB_MITGLIED_SEIT",	"BAHNCARD",	"ZAHLUNGSWEISE",	"JAHRESKARTE_OEPNV",	"MOTORRAD_BESITZER",	"AUTOMOBILCLUB",	"SFKLASSE_VOLLKASKO",	"SFKLASSE_HAFTPFLICHT",	"STELLPLATZ_ABSCHLIESSBAR",	"NAECHTLICHER_STELLPLATZ",	"NUTZUNGSWEISE",	"JAEHRLICHE_FAHRLEISTUNG",	"TSN",	"ERSTZULASSUNG",	"HSN",	"FINANZIERUNGSART",	"ZULASSUNG_AUF_VERSICHERUNGSNEHM",	"STADT",	"KENNZEICHEN",	"PLZ_DES_HALTER",	"SELBSTGENUTZTES_WOHNEIGENTUM",	"ART_DES_WOHNEIGENTUM",	"GEBURTSDATUM",	"FAMILIENSTAND",	"NATIONALITAET",	"GESCHLECHT",	"FUEHRERSCHEIN_ERWORBEN_AM",	"VORSCHAEDEN0_typeKH",	"VORSCHAEDEN0_typetk",	"VORSCHAEDEN0_month",	"VORSCHAEDEN0_year",	"VORSCHAEDEN1_typetk",	"VORSCHAEDEN1_month",	"VORSCHAEDEN1_year",	"VORSCHAEDEN2_typevk",	"VORSCHAEDEN2_month",	"VORSCHAEDEN2_year",	"adacid",	"name",	"marke",	"modell",	"preis",	"getriebeart",	"antriebsart",	"Fahrzeugklasse",	"co2klasse",	"kw",	"ps",	"tueranzahl",	"Motorart",	"Kraftstoffart",	"Motorbauart",	"Schadstoffklasse",	"Karosserie",	"Sitzanzahl",	"typklasseh_num",	"typklassetk_num",	"typklassevk_num",	"hubraum2",	"drehmoment2",	"breite2",	"radstand2",	"laenge2",	"hoehe2",	"leergewicht2",	"gesamtgewicht2",	"zuladung2",	"kofferraumvolumen_num",	"hoechstgeschwindigkeit2",	"verbrauchgesamt2",	"verbrauchausserorts2",	"verbrauchinnerorts2",	"beschleunigung2",	"tank2",	"kfzsteuer2",	"anzahlgaenge2",	"anzahlzylinder2",	"co2_wert",	"modellstart_y"]
+##################################################
+#run tree
+##################################################
+strs,resm=dtm(dtmtable,sett)
+@test typeof(resm.modelstats)==DataFrame
+@test 1==1
+
+##################################################
+#run boosting
+##################################################
+
+sett.niter=10
+sett.model_type="boosted_tree"
+strs,resm2=dtm(dtmtable,sett)
+@test typeof(resm2.modelstats)==DataFrame
+@test 1==1
+
 
 di9=load("C:\\Users\\bernhard.konig\\Documents\\ASync\\irobot\\data_tmp\\prepped_dtm2table.jld");dtmtable=di9["dtmtable"];this_dt2msett=di9["this_dt2msett"];
 
