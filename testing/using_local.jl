c4d75795c1a3554085802c0065d382a7cf9e5abb
bernhard <github@diro.ch>
2018-06-23 20:04:29 +0200

    added a file

---
+++ testing/using_local.jl
@@ -0,0 +1,24 @@
+cd(Pkg.dir("DecisionTrees"))
+import Core 
+import Dates
+println(Dates.now())
+import Random
+import Distributed
+import DelimitedFiles
+import PyCall
+import DataFrames
+import DataFrames: DataFrame
+import JLD2
+import OnlineStats
+import ProgressMeter
+import StatsBase
+
+#try PyCall
+global const pyAB = PyCall.PyNULL()
+global const pyCD = PyCall.PyNULL()
+copy!(pyAB, PyCall.pyimport_conda("pandas","pandas"))
+copy!(pyCD, PyCall.pyimport_conda("xlsxwriter","xlsxwriter"))
+
+#using dt:
+using DecisionTrees
+println(Dates.now())
