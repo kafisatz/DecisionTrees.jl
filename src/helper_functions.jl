e1d8986d8e2b0f58d413fbc8f3ddfbb4c068f50d
kafisatz <>
2018-06-24 06:25:59 +0200

    changed docs and signature

--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -1495,9 +1495,8 @@ end
 
 """
 returns l and r indices (l corresponds to all elements of trnidx that 'match' subset)
-This verison of the function is for String / Categorical variables
 """
-function lrIndices(trnidx::Vector{Int},f::PooledArray{String,T,1},subset::Array) where T<:Unsigned
+function lrIndices(trnidx::Vector{Int},f,subset::Array) where T<:Unsigned
 	l=Vector{Int}(undef,0)
 	r=Vector{Int}(undef,0)
 	sizehint!(l,length(trnidx))
@@ -1516,7 +1515,6 @@ end
 
 """
 returns l and r indices (l corresponds to all elements of trnidx that 'match' subset)
-This verison of the function is for String / Categorical variables
 """
 function lrIndicesForContiguousSubset(trnidx::Vector{Int},f,subset::Array) where T<:Unsigned
 	l=Vector{Int}(undef,0)
