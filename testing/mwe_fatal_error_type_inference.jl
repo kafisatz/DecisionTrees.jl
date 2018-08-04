1eea1d0b4dd1773d176f0129a45e63084f593212
bernhard <5026865+kafisatz@users.noreply.github.com>
2018-08-04 10:39:42 +0200

    added mwe

--- testing/mwe_fatal_error_type_inference.jl
+++ testing/mwe_fatal_error_type_inference.jl
@@ -1,17 +1,21 @@
+#sample script to reproduce
+#ERROR: fatal error in type inference (type bound)
+
+#it seems related to Union{mseSplit,sseSplit}
+#(search the code for that expression)
+#I am quite sure that this did not happen on 0.7beta2
+
+import Distributed
+using Pkg
+pkg"develop https://github.com/kafisatz/DecisionTrees.jl#master"
+pkg"activate DecisionTrees.jl"
+Distributed.@everywhere using DecisionTrees
+
+#if the above does not work, maybe you need to clone the git
 #git clone --depth=50 --branch=master https://github.com/kafisatz/DecisionTrees.jl.git 
 #cd kafisatz/DecisionTrees.jl
 
 @assert VERSION>=v"0.7.0-rc2.0"
-
-import Distributed
-
-using Pkg
-pkg"activate DecisionTrees.jl"
-Distributed.@everywhere using DecisionTrees
-
-dr="C:\\Users\\bernhard.konig\\Documents\\ASync\\home\\Code\\Julia\\DecisionTrees.jl"
-isdir(dr)&&cd(dr)
-pwd()
 
 Distributed.@everywhere import CSV
 Distributed.@everywhere import DataFrames
@@ -20,6 +24,11 @@ Distributed.@everywhere import DataFrames: DataFrame
 ##############################
 #Read the data
 ##############################
+
+dr="C:\\Users\\bernhard.konig\\Documents\\ASync\\home\\Code\\Julia\\DecisionTrees.jl"
+isdir(dr)&&cd(dr)
+pwd()
+
 @warn("""This assumes pwd() == Pkg.dir("DecisionTrees.jl")""") 
 datafile=joinpath("data","freMTPL2","freMTPL2.csv")
 @assert isfile(datafile) "You need to unzip the file freMTPL2.zip in the folder data/freMTPL2"
@@ -45,18 +54,40 @@ dtmtable,sett,dfprepped=prepare_dataframe_for_dtm!(fullData,keycol="IDpol",trnva
 
 #run a few models
 
-#this runs through as expected
+#this runs as expected
     updateSettings!(sett,minWeight=-0.03,model_type="build_tree",calculatePoissonError=true)
     resultingFiles,resM=dtm(dtmtable,sett)
 
-#this runs through as expected
+#this runs as expected
     updateSettings!(sett,crit="sse",model_type="build_tree")
     resultingFiles,resM=dtm(dtmtable,sett)
 
-#this runs through as expected
+#this runs as expected
     updateSettings!(sett,crit="sse",model_type="build_tree")
     resultingFiles,resM=dtm(dtmtable,sett)
 
 #this one fails!
     updateSettings!(sett,crit="sse",model_type="boosted_tree")
     resultingFiles,resM=dtm(dtmtable,sett)
+
+#=
+# this is the error I get
+
+"""
+ERROR: fatal error in type inference (type bound)
+Stacktrace:
+ [1] _typed_hcat(::Type{Float64}, ::Tuple{Array{Int64,1},Array{Int64,1},Array{Float64,1},Array{Float64,1},Array{Float64,1},Array{Float64,1},Array{Float64,1},Array{Float64,1}}) at .\abstractarray.jl:1255
+ [2] typed_hcat at .\abstractarray.jl:1243 [inlined]
+ [3] hcat at C:\cygwin\home\Administrator\buildbot\worker\package_win64\build\usr\share\julia\stdlib\v0.7\SparseArrays\src\sparsevector.jl:1063 [inlined]
+ [4] macro expansion at .\logging.jl:307 [inlined]
+ [5] macro expansion at .\util.jl:156 [inlined]
+ [6] macro expansion at C:\Users\bernhard.konig\Documents\ASync\home\Code\Julia\DecisionTrees.jl\src\boosting.jl:89 [inlined]
+ [7] macro expansion at .\util.jl:156 [inlined]
+ [8] macro expansion at C:\Users\bernhard.konig\Documents\ASync\home\Code\Julia\DecisionTrees.jl\src\helper_functions.jl:1469 [inlined]
+ [9] boosted_tree(::DecisionTrees.DTMTable, ::ModelSettings) at C:\Users\bernhard.konig\Documents\ASync\home\Code\Julia\DecisionTrees.jl\src\boosting.jl:82
+ [10] run_model_actual(::DecisionTrees.DTMTable, ::ModelSettings, ::String) at C:\Users\bernhard.konig\Documents\ASync\home\Code\Julia\DecisionTrees.jl\src\runmodel.jl:467
+ [11] #dtm#110 at C:\Users\bernhard.konig\Documents\ASync\home\Code\Julia\DecisionTrees.jl\src\runmodel.jl:798 [inlined]
+ [12] dtm(::DecisionTrees.DTMTable, ::ModelSettings) at C:\Users\bernhard.konig\Documents\ASync\home\Code\Julia\DecisionTrees.jl\src\runmodel.jl:798
+ [13] top-level scope at none:0
+ """
+ =#
