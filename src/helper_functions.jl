bfe787df103ef6237332456c7b8f461fc93c252e
Bernhard König <github@diro.ch>
2021-05-06 16:02:56 +0200

    attempt to fix df conversion

--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -2172,8 +2172,7 @@ function createTrnValStats!(trnidx::Vector{Int}, validx::Vector{Int}, sett::Mode
     poiErr_rowval = ["Average Poisson Error Val" poissonErrVal repeat([""], 1, size(thisres, 2) - 2)]
     thisres = [thisres;poiErr_rowval];overallstats = [overallstats;poiErr_rowval];
 
-    @show thisres 
-    thisresdf = convert(DataFrame,convert(Array{String},thisres))
+    thisresdf = DataFrame([something.(col, missing) for col in eachcol(thisres[2:end, :])], Symbol.(thisres[1, :]))
     modelStatisticsSheet = ExcelSheet(nameOfModelStatisticsSheet, thisresdf)
     modelsettingsSheet = ExcelSheet(nameOfSettingsSheet, convert(DataFrame, writeAllFieldsToArray(sett)))
     xlData.sheets = [modelsettingsSheet,modelStatisticsSheet]
